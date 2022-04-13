283ccaabd4524a66e986a189043c9f805a603a57
Mubin <mubin.ajkerdeal@gmail.com>
2022-04-13 16:20:20 +0600

    Initial commit

---
+++ src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,59 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+
+public class Student {
+    int id;
+    String name;
+    String email;
+    LocalDate dob;
+    int age;
+
+    public Student(int id, String name, String email, LocalDate dob, int age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
